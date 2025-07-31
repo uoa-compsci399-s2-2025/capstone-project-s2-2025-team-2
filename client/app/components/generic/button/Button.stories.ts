cc83e0aca7781ca4cd913e125cb78f2288ed7820
ganznz <loganbellinghamlb@gmail.com>
2025-07-31 15:31:57 +1200

    refactor: refactored button example to fit our techstack

--- client/app/components/generic/button/Button.stories.ts
+++ client/app/components/generic/button/Button.stories.ts
@@ -1,22 +1,22 @@
-import type { Meta, StoryObj } from '@storybook/nextjs';
+import type { Meta, StoryObj } from "@storybook/nextjs";
 
-import { fn } from 'storybook/test';
+import { fn } from "storybook/test";
 
-import { Button } from './Button';
+import Button from "./Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
@@ -26,29 +26,32 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const SmallButton: Story = {
   args: {
-    primary: true,
-    label: 'Button',
+    size: "small",
+    label: "I am a small button",
+    backgroundColor: "#ff5555",
   },
 };
 
-export const Secondary: Story = {
+export const MediumButton: Story = {
   args: {
-    label: 'Button',
+    size: "medium",
+    label: "I am a medium button",
+    backgroundColor: "#ea49f2",
   },
 };
 
-export const Large: Story = {
+export const LargeButton: Story = {
   args: {
-    size: 'large',
-    label: 'Button',
+    size: "large",
+    label: "Ray Zhao 🥵🥵🥵🥵",
+    backgroundColor: "#34ba32",
   },
 };
 
-export const Small: Story = {
+export const DefaultButton: Story = {
   args: {
-    size: 'small',
-    label: 'Button',
+    label: "I don't have specified props",
   },
 };
