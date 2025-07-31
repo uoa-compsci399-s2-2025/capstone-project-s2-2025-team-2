bcfd1d741b696b134e8dbb9919fcd0fefe4d26dc
Logan <78389560+ganznz@users.noreply.github.com>
2025-07-31 16:24:55 +1200

    Frontend setup storybook (#10)
    * feat: install storybook library
    
    * refactor: refactored button example to fit our techstack
    
    * chore: updated storybook config + story files now render tailwind classes
    
    * chore: rename root dir cmds for consistency
    
    * chore: story files now use ``.story,ts`` instead of ``.stories.ts``
    
    * fix: remove ``: any`` type so lint ci pipeline passes
    
    * chore: delete storybook default static files
    
    * feat: format
    
    ---------
    
    Co-authored-by: zlrkw11 <64724157+zlrkw11@users.noreply.github.com>
    Co-authored-by: zlrkw11 <ray040406@gmail.com>

---
+++ client/app/components/generic/button/Button.tsx
@@ -0,0 +1,58 @@
+import { ButtonHTMLAttributes } from "react"
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  backgroundColor?: string
+  size?: "small" | "medium" | "large"
+  label: string
+}
+
+// base button component w/ common styling/functionality across all variants
+const BaseButton = ({
+  backgroundColor = "#b3b3b3",
+  label,
+  className,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      type="button"
+      className={[
+        "inline-block cursor-pointer border-0 rounded-full font-bold",
+        className,
+      ].join(" ")}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  )
+}
+
+// btn variants
+const SmallButton = (props: ButtonProps) => (
+  <BaseButton {...props} className="py-2.5 px-4 text-xs" />
+)
+
+const MediumButton = (props: ButtonProps) => (
+  <BaseButton {...props} className="py-3 px-5 text-sm" />
+)
+
+const LargeButton = (props: ButtonProps) => (
+  <BaseButton {...props} className="py-3.5 px-6 text-base" />
+)
+
+// exported btn component to be used
+const Button = ({ size = "medium", ...props }: ButtonProps) => {
+  switch (size) {
+    case "small":
+      return <SmallButton {...props} />
+    case "medium":
+      return <MediumButton {...props} />
+    case "large":
+      return <LargeButton {...props} />
+    default:
+      return <MediumButton {...props} />
+  }
+}
+
+export default Button
