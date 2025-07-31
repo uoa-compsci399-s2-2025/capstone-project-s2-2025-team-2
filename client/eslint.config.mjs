dc24cb7c2634fe925aa29921549a69a765b6c594
ganznz <loganbellinghamlb@gmail.com>
2025-07-31 14:06:10 +1200

    feat: install storybook library

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -2,6 +2,9 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -9,8 +12,9 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = {
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+};
+const storybookConfig = { ...storybook.configs["flat/recommended"] };
 
-export default eslintConfig;
+export default [eslintConfig, storybookConfig];
