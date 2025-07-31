989cadd617e6e47d06321d9341ee1b9e980a5c63
ganznz <loganbellinghamlb@gmail.com>
2025-07-31 15:34:44 +1200

    chore: updated storybook config + story files now render tailwind classes

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,26 +1,21 @@
-import type { StorybookConfig } from '@storybook/nextjs';
+import type { StorybookConfig } from "@storybook/nextjs";
 
-import { join, dirname } from "path"
+import { join, dirname } from "path";
 
 /**
-* This function is used to resolve the absolute path of a package.
-* It is needed in projects that use Yarn PnP or are set up within a monorepo.
-*/
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
+  return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [],
-  "framework": {
-    "name": getAbsolutePath('@storybook/nextjs'),
-    "options": {}
+  stories: ["../app/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [],
+  framework: {
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {},
   },
-  "staticDirs": [
-    "..\\public"
-  ]
+  staticDirs: ["..\\public"],
 };
 export default config;
