ff304ac29792b6dafb544ec22a9631040140b12f
zlrkw11 <64724157+zlrkw11@users.noreply.github.com>
2025-08-14 23:15:10 +1200

    Frontend tailwind config (#14)
    * feat: set up root colors
    
    * feat: apply colors to inline tailwindcss
    
    * feat: tested colors to be working
    
    * feat: prettier ignore for generated routes
    
    * feat: color set

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="font-sans bg-salmon grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start bg-orange">
         <Image
           className="dark:invert"
           src="/next.svg"
