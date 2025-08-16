9163ee57bc14dc6199378c50ed0d4cf531f20ff2
ZL <ray040406@gmail.com>
2025-08-17 01:10:32 +1200

    feat: color update, disabled button

---
+++ client/app/components/generic/button/disabled/DisabledButton.tsx
@@ -0,0 +1,52 @@
+import { ButtonHTMLAttributes } from "react"
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  backgroundColor?: string
+  size?: "small" | "medium" | "large"
+  label: string
+}
+
+// base button component w/ common styling/functionality across all variants
+const BaseButton = ({ label, className, ...props }: ButtonProps) => {
+  return (
+    <button
+      type="button"
+      className={[
+        "inline-block rounded-[8px] font-sans bg-muted",
+        className,
+      ].join(" ")}
+      {...props}
+    >
+      <h5 className="text-secondary">{label}</h5>
+    </button>
+  )
+}
+
+// btn variants
+const SmallButton = (props: ButtonProps) => (
+  <BaseButton {...props} className="py-1.5 px-3" />
+)
+
+const MediumButton = (props: ButtonProps) => (
+  <BaseButton {...props} className="py-3 px-5" />
+)
+
+const LargeButton = (props: ButtonProps) => (
+  <BaseButton {...props} className="py-4.5 px-7" />
+)
+
+// exported btn component to be used
+const DisabledButton = ({ size = "medium", ...props }: ButtonProps) => {
+  switch (size) {
+    case "small":
+      return <SmallButton {...props} />
+    case "medium":
+      return <MediumButton {...props} />
+    case "large":
+      return <LargeButton {...props} />
+    default:
+      return <MediumButton {...props} />
+  }
+}
+
+export default DisabledButton
