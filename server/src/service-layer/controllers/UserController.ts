5c89484925e4e625bd65e29021aafa4d4ab775fb
ZL <ray040406@gmail.com>
2025-08-20 01:16:35 +1200

    feat: getUser(id) function in controller and implementation

--- server/src/service-layer/controllers/UserController.ts
+++ server/src/service-layer/controllers/UserController.ts
@@ -1,1 +1,19 @@
+import { User } from "data-layer/models/models"
+import { Controller, Get, Path, Route, SuccessResponse } from "tsoa"
+import { UserService } from "../../data-layer/services/UserService"
+@Route("users")
+export class UserController extends Controller {
+  private userService: UserService
 
+  constructor() {
+    super()
+    this.userService = new UserService()
+  }
+
+  @SuccessResponse("200", "Found")
+  @Get("{userId}")
+  public async getUser(@Path() id: string): Promise<User | null> {
+    const user = await this.userService.getUser(id)
+    return user
+  }
+}
