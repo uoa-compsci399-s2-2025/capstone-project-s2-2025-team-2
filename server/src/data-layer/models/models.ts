698b5a558102db6eb00a897fc2ee32beca3b887c
ZL <ray040406@gmail.com>
2025-08-20 00:56:55 +1200

    chore: clear out default files

--- server/src/data-layer/models/models.ts
+++ server/src/data-layer/models/models.ts
@@ -1,12 +1,5 @@
 export interface User {
   id: number
+  username: string
   email: string
-  name: string
-  phoneNumbers: string[]
-}
-
-export interface Admin extends User {
-  title: string
-  admin_id: number
-  permissions?: string[]
 }
