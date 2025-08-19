07da007823b4f0fd7f7f7011cd4b47f40e33abcb
ZL <ray040406@gmail.com>
2025-08-20 01:12:30 +1200

    feat: setting up connection with adapters

--- server/src/data-layer/services/UserService.ts
+++ server/src/data-layer/services/UserService.ts
@@ -1,1 +1,15 @@
+import FirestoreCollections from "data-layer/adapters/FirestoreCollections"
+import { User } from "data-layer/models/models"
 
+export class UserService {
+  async getUser(id: string): Promise<User | null> {
+    const userRef = FirestoreCollections.users.doc(id)
+    const user = await userRef.get()
+    if (!user.exists) {
+      console.log(`User - ${id} is not found`)
+      return null
+    }
+    console.log(user.data())
+    return user.data() as User
+  }
+}
