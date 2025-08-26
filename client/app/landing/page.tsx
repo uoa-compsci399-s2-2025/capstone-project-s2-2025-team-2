cdc9feef4363c931f2405e18a03334c8b57fc950
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-08-26 14:29:10 +1200

    restructure files

---
+++ client/app/landing/page.tsx
@@ -0,0 +1,16 @@
+import Header from "../components/composite/landing/Header";
+import Landing from "../components/composite/landing/Landing";
+
+export default function page() {
+  return (
+    <div
+      className="
+    bg-tint pb-[2rem] dark:bg-black min-h-[100vh]
+    md:p-[3rem]
+  "
+    >
+      <Header/>
+      <Landing/>
+    </div>
+  )
+}
