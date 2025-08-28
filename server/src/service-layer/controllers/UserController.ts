028fc6b3fa1342262675bf838d09c63fe6128cb1
ZL <ray040406@gmail.com>
2025-08-29 01:08:42 +1200

    feat: call get_all method from user controller

--- server/src/service-layer/controllers/UserController.ts
+++ server/src/service-layer/controllers/UserController.ts
@@ -1,6 +1,10 @@
 import { User } from "../../data-layer/models/User"
-import { Controller, Get, Route, SuccessResponse } from "tsoa"
+import { Controller, Get, Route, SuccessResponse, Security, Query } from "tsoa"
 import { UserService } from "../../data-layer/services/UserService"
+import { ReagentService } from "../../data-layer/services/ReagentService"
+import { Reagent } from "../../data-layer/models/Reagent"
+import { ReagentCategory } from "../../data-layer/models/Reagent"
+
 @Route("users")
 export class UserController extends Controller {
   @SuccessResponse("200", "Users retrieved successfully")
@@ -10,5 +14,31 @@ export class UserController extends Controller {
     const users = await new UserService().getAllUsers()
     console.log(`Returning ${users.length} users`)
     return users
+  }
+
+  /**
+   * Get all reagents with an option to filter them by category.
+   * User must be authenticated to access this endpoint
+   * @param category - The list of all categories to fetch reagents from.
+   * @returns Promise<Reagent[]> - The list of all reagents filtered.
+   */
+  @SuccessResponse("200", "All reagents returned successfully")
+  @Get()
+  @Security("jwt")
+  public async getReagents(
+    @Query() category?: ReagentCategory[],
+  ): Promise<Reagent[]> {
+    try {
+      if (category) {
+        const reagents = await new ReagentService().getReagentsByCategory(
+          category,
+        )
+        return reagents
+      }
+      const reagents = await new ReagentService().getAllReagents()
+      return reagents
+    } catch (err) {
+      throw new Error("Failed to fetch reagents: " + (err as Error).message)
+    }
   }
 }
