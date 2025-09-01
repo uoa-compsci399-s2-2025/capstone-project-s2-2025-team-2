c48c3fcc1d23b53dd6b7585efe36db5eb96cdd78
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-09-02 11:54:47 +1200

    reagentCard

---
+++ client/app/components/composite/marketplace/ReagentCard.tsx
@@ -0,0 +1,18 @@
+const ReagentCard = () => {
+    return ( 
+        <div className="
+            md:w-[13rem] md:h-[17rem] p-[0.2rem] 
+            h-[6rem] mx-[2rem] mb-[1.5rem]
+            border-blue-secondary border-solid border-[1.5px] rounded-[15px]
+        ">
+            <div className="
+                h-full
+                border-dark-gray border-solid border-[1.5px] rounded-[12px]
+            ">
+                inner
+            </div>
+        </div>
+     );
+}
+ 
+export default ReagentCard;
--- client/app/marketplace/page.tsx
+++ client/app/marketplace/page.tsx
@@ -1,7 +1,13 @@
+import ReagentCard from "../components/composite/marketplace/ReagentCard";
+
 const Marketplace = () => {
     return ( 
-        <div>
+        <div className="bg-white">
             Marketplace
+            <ReagentCard/>
+            <ReagentCard/>
+            <ReagentCard/>
+            <ReagentCard/>
         </div>
      );
 }
