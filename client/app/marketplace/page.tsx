9ac9e0185e75a5928f560b70cd57cd3e557d475b
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-09-02 16:53:18 +1200

    feat:desktop card dark mode

--- client/app/marketplace/page.tsx
+++ client/app/marketplace/page.tsx
@@ -2,7 +2,7 @@ import ReagentCard from "../components/composite/marketplace/ReagentCard";
 
 const Marketplace = () => {
     return ( 
-        <div className="bg-white flex w-[100vw] flex-wrap pt-[2rem]">
+        <div className="bg-white dark:bg-black flex w-[100vw] flex-wrap pt-[2rem]">
             Marketplace
             <ReagentCard/>
             <ReagentCard/>
