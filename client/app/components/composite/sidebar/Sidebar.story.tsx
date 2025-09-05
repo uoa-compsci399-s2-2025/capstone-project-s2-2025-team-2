dbcb2dc39fcc876cdc897c1cf5c1db63061c952d
ZL <ray040406@gmail.com>
2025-09-05 13:46:10 +1200

    feat: sidebar storybook import naming

--- client/app/components/composite/sidebar/Sidebar.story.tsx
+++ client/app/components/composite/sidebar/Sidebar.story.tsx
@@ -24,3 +24,4 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const DefaultSidebar: Story = {}
