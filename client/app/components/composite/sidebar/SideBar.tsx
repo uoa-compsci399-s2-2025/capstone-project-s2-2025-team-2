5266c0d5c5da3832b87d095cc5fbe3b21e43d231
ZL <ray040406@gmail.com>
2025-09-05 13:43:35 +1200

    feat: sidebar storybook file

--- client/app/components/composite/sidebar/SideBar.tsx
+++ client/app/components/composite/sidebar/SideBar.tsx
@@ -1,13 +1,22 @@
 "use client"
 import Link from "next/link"
-import { ShoppingCartIcon, UserCircleIcon, ArrowLeftStartOnRectangleIcon } from "@heroicons/react/24/outline"
+import {
+  ShoppingCartIcon,
+  UserCircleIcon,
+  ArrowLeftStartOnRectangleIcon,
+} from "@heroicons/react/24/outline"
 import Button from "../../generic/button/regular/Button"
 
 const Sidebar = () => {
   const links = [
-    { href: "/userprofile", label: "Profile", icon: UserCircleIcon, isButton: true },
+    {
+      href: "/userprofile",
+      label: "Profile",
+      icon: UserCircleIcon,
+      isButton: true,
+    },
     { href: "/marketplace", label: "Marketplace", icon: ShoppingCartIcon },
-  ];
+  ]
 
   return (
     <div className="fixed pt-12 w-48 h-full bg-[var(--primary)]">
@@ -15,11 +24,15 @@ const Sidebar = () => {
         <div key={href} className="mb-2">
           {isButton ? (
             <Link href={href}>
-          <div className="w-[8.5rem] mx-auto">
-        <Button className="py-1" size="small" icon={Icon} label={label} iconPosition="left"   />
-
-          </div>
-
+              <div className="w-[8.5rem] mx-auto">
+                <Button
+                  className="py-1"
+                  size="small"
+                  icon={Icon}
+                  label={label}
+                  iconPosition="left"
+                />
+              </div>
             </Link>
           ) : (
             <Link
@@ -31,21 +44,20 @@ const Sidebar = () => {
             </Link>
           )}
           {index === 0 && (
-        <div className="my-2 h-[2px] w-full bg-gradient-to-r bg-gradient-to-r from-transparent from-15% via-blue-primary via-50% to-transparent to-85%" />
-      )}
+            <div className="my-2 h-[2px] w-full bg-gradient-to-r bg-gradient-to-r from-transparent from-15% via-blue-primary via-50% to-transparent to-85%" />
+          )}
         </div>
       ))}
 
-     <button
-            onClick={() => alert("Signing out...")}
-            className="fixed bottom-0 text-[var(--white)] hover:bg-red-600"
-          >
-               <div className="flex items-center gap-2 p-2">
+      <button
+        onClick={() => alert("Signing out...")}
+        className="fixed bottom-0 text-[var(--white)] hover:bg-red-600"
+      >
+        <div className="flex items-center gap-2 p-2">
           <ArrowLeftStartOnRectangleIcon className="w-5 h-5" />
-            Sign Out
-               </div>
-
-     </button>
+          Sign Out
+        </div>
+      </button>
     </div>
   )
 }
--- client/app/components/composite/sidebar/Sidebar.story.tsx
+++ client/app/components/composite/sidebar/Sidebar.story.tsx
@@ -2,12 +2,12 @@ import type { Meta, StoryObj } from "@storybook/nextjs"
 
 import { fn } from "storybook/test"
 
-import ReagentCard from "./ReagentCard"
+import Sidebar from "./SideBar"
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Example/ReagentCard",
-  component: ReagentCard,
+  title: "Example/Sidebar",
+  component: Sidebar,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
@@ -18,22 +18,9 @@ const meta = {
   argTypes: {},
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: {},
-} satisfies Meta<typeof ReagentCard>
+} satisfies Meta<typeof Sidebar>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const DefaultReagentCard: Story = {
-  args: {
-    name: "Ethanol",
-    tags: ["liquid", "safe", "alchohol", "flammable"],
-    location: "UoA, Auckland",
-    expiryDate: "25/10/2026",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4R4SrdU7Rbyr3DqFDmKBsFH9t9lqjLcJetw&s",
-    quantity: "1L",
-    formula: "C2H6O",
-    description: "This is an example reagent",
-  },
-}
