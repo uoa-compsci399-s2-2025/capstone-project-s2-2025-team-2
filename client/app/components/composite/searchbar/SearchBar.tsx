8f82283f1a28b1ed46a9ad16603c7c9cc1fe5d66
vi0letchen <violethongjinchen@gmail.com>
2025-09-07 20:19:37 +1200

    feat:search bar styling

--- client/app/components/composite/searchbar/SearchBar.tsx
+++ client/app/components/composite/searchbar/SearchBar.tsx
@@ -26,14 +26,14 @@ export default function SearchBar({
   return (
     <form
       onSubmit={handleSearch}
-      className="flex items-center mx-auto px-10 py-1 w-full shadow-md"
+      className="flex items-center mx-auto px-10 py-5 w-full"
     >
       {/* Filter */}
-      <div className="relative w-1/5"> 
+      <div className="relative w-1/5 min-w-27"> 
       <select
         value={filter}
         onChange={(e) => setFilter(e.target.value)}
-        className="appearance-none bg-primary text-secondary px-2 py-1 outline-none cursor-pointer w-full"
+        className="appearance-none bg-primary rounded-tl-lg rounded-bl-lg text-secondary px-2 py-1 outline-none cursor-pointer w-full shadow-lg"
       >
         <option value="all">Filter by...</option>
         <option value="category">Category</option>
@@ -41,13 +41,13 @@ export default function SearchBar({
         <option value="tag">Tag</option>
       </select>
       <ChevronDownIcon
-    className="absolute right-2 top-1/2 -translate-y-1/2 h-5 w-5 text-secondary pointer-events-none"
+    className="absolute right-2 top-1/2 -translate-y-1/2 h-5 w-5 text-secondary pointer-events-none shadow-lg"
   />        
       </div>
 
 
       {/* Search input */}
-      <div className="bg-primary flex items-center flex-1 mx-2 px-2 py-1">
+      <div className="bg-primary flex items-center flex-1 mx-2 px-2 py-1 shadow-md">
         <MagnifyingGlassIcon className="h-5 w-5 text-secondary mr-2" />
         <input
           type="text"
@@ -59,13 +59,13 @@ export default function SearchBar({
       </div>
 
       {/* Sort */}
-      <div className="relative w-1/5">
+      <div className="relative w-1/5 min-w-25">
       <select
         value={sort}
         onChange={(e) =>
           setSort(e.target.value as "newest" | "oldest" | "name" | "")
         }
-        className="appearance-none bg-primary text-secondary px-2 py-1 outline-none cursor-pointer w-full"
+        className="appearance-none bg-primary rounded-tr-lg rounded-br-lg text-secondary px-2 py-1 outline-none cursor-pointer w-full shadow-lg"
       >
         <option value="">Sort by...</option>
         <option value="newest">Newest</option>
