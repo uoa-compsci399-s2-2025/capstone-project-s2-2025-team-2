a98df9b3f1de773840326ab515b8b83ebdb1f89a
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-09-10 20:43:02 +1200

    feat:seller contact info section

--- client/app/components/composite/reagentview/SellerContact.tsx
+++ client/app/components/composite/reagentview/SellerContact.tsx
@@ -3,12 +3,16 @@ import { FaRegStar } from "react-icons/fa";
 
 const SellerContact = () => {
     return ( 
-        <div className="flex">
-            <div className="bg-yellow-600">ProfilePic</div>
-            <div>
-                <h4 className="text-midnight">Violet Chen</h4>
-                <span className="flex items-center"><LuHouse className="text-midnight"/><p className="text-midnight">University of Auckland, City Centre</p></span>
-                <span className="text-gold items-center flex"><FaRegStar />99(99.9% Positive Feedback)</span>
+        <div className="flex flex-row items-center">
+            <div className="bg-yellow-600 rounded-[50%] flex flex-col justify-center items-center h-[4rem] w-[4rem]">PFP</div>
+            <div className="ml-[1rem]">
+                <h4 className="text-midnight text-[1rem] font-bold">Violet Chen</h4>
+                <span className="flex items-center text-[0.8rem] text-midnight font-bold"><LuHouse/>
+                    University of Auckland, City Centre
+                </span>
+                <span className="text-gold items-center flex text-[0.8rem] font-bold"><FaRegStar />
+                    99(99.9% Positive Feedback)
+                </span>
             </div>
         </div>
      );
