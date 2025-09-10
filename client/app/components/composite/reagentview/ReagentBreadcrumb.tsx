556cf3a4829e7fd814d62fe2a3cb63a4d1af90b8
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-09-11 11:45:51 +1200

    format

--- client/app/components/composite/reagentview/ReagentBreadcrumb.tsx
+++ client/app/components/composite/reagentview/ReagentBreadcrumb.tsx
@@ -1,9 +1,11 @@
 const ReagentBreadcrumb = () => {
-    return ( 
-        <div>
-            <h5 className="font-bold text-dark-gray text-[0.8rem]">Reagents / Listings / Hydrogen Peroxide 5%</h5>
-        </div>
-     );
+  return (
+    <div>
+      <h5 className="font-bold text-dark-gray text-[0.8rem]">
+        Reagents / Listings / Hydrogen Peroxide 5%
+      </h5>
+    </div>
+  )
 }
- 
-export default ReagentBreadcrumb;
+
+export default ReagentBreadcrumb
--- client/app/components/composite/reagentview/SellerContact.tsx
+++ client/app/components/composite/reagentview/SellerContact.tsx
@@ -1,24 +1,30 @@
-import { LuHouse } from "react-icons/lu";
-import { FaRegStar } from "react-icons/fa";
+import { LuHouse } from "react-icons/lu"
+import { FaRegStar } from "react-icons/fa"
 
 const SellerContact = () => {
-    return ( 
-        <div className="flex flex-row items-center">
-            <div className="bg-yellow-600 rounded-[50%] flex flex-col justify-center items-center h-[4rem] w-[4rem]">PFP</div>
-            <div className="mx-[1.5rem]">
-                <h4 className="text-midnight text-[1rem] font-bold">Violet Chen</h4>
-                <span className="flex items-center text-[0.8rem] text-midnight font-bold"><LuHouse className="mr-[0.3rem]"/>
-                    University of Auckland, City Centre
-                </span>
-                <span className="text-gold items-center flex text-[0.8rem] font-bold"><FaRegStar className="mr-[0.3rem]" />
-                    99 (99.9% Positive Feedback)
-                </span>
-            </div>
-            <button className="bg-blue-primary text-white font-sans rounded-[5px]">
-                <h4 className="text-[0.8rem] font-semibold px-[2.5rem] py-[0.5rem]">Contact Violet</h4>
-            </button>
-        </div>
-     );
+  return (
+    <div className="flex flex-row items-center">
+      <div className="bg-yellow-600 rounded-[50%] flex flex-col justify-center items-center h-[4rem] w-[4rem]">
+        PFP
+      </div>
+      <div className="mx-[1.5rem]">
+        <h4 className="text-midnight text-[1rem] font-bold">Violet Chen</h4>
+        <span className="flex items-center text-[0.8rem] text-midnight font-bold">
+          <LuHouse className="mr-[0.3rem]" />
+          University of Auckland, City Centre
+        </span>
+        <span className="text-gold items-center flex text-[0.8rem] font-bold">
+          <FaRegStar className="mr-[0.3rem]" />
+          99 (99.9% Positive Feedback)
+        </span>
+      </div>
+      <button className="bg-blue-primary text-white font-sans rounded-[5px]">
+        <h4 className="text-[0.8rem] font-semibold px-[2.5rem] py-[0.5rem]">
+          Contact Violet
+        </h4>
+      </button>
+    </div>
+  )
 }
- 
-export default SellerContact;
+
+export default SellerContact
