52871143127ca7cfc0198e404f91f9bf566f75b5
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-09-10 20:21:07 +1200

    feat:tags

---
+++ client/app/components/composite/reagentview/Tag.tsx
@@ -0,0 +1,12 @@
+const Tag = () => {
+    return ( 
+        <div className="
+            mx-[0.4rem] bg-light-gray text-dark-gray px-[0.8rem] rounded-[0.7rem] font-family-sans
+            tracking-widest
+        ">
+            Tag
+        </div>
+     );
+}
+ 
+export default Tag;
