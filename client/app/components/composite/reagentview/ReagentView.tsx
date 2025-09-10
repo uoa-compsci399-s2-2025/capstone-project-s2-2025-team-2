785542e8176e4b46318d24e6b31e21f74d7e0def
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-09-10 20:21:07 +1200

    feat:tags

--- client/app/components/composite/reagentview/ReagentView.tsx
+++ client/app/components/composite/reagentview/ReagentView.tsx
@@ -1,11 +1,11 @@
 import { FaRegClock } from "react-icons/fa";
 import { LuClockAlert } from "react-icons/lu";
 import { LuHouse } from "react-icons/lu";
+import Tag from "./Tag";
 
 const ReagentView = () => {
     return ( 
-        <div className="bg-white ">
-            Reagent View
+        <div className="bg-white h-[100vh] font-family-sans">
             <div className="flex flex-col items-center">
                 <h1 className="text-black tracking-wider">Hydrogen Peroxide 5%</h1>
                 <div className="flex justify-around w-[43rem] mx-auto mt-[1rem]">
@@ -16,21 +16,21 @@ const ReagentView = () => {
             </div>
             <div className="flex mt-[2rem]">
                 <div className="bg-amber-300 flex-[1]">Images</div>
-                <div className="border-red-600 border-2 flex-[1.5] p-[1rem]">
-                    <div className="m-1 bg-green-300 rounded-[10px]">
+                <div className="flex-[1.5] p-[1rem]">
+                    <div className="m-1 bg-green-300 rounded-[10px] shadow-[0_4px_6px_rgba(0,0,0,0.4)]">
                         <div className="text-center py-[1rem]">
                             <h4 className="text-green-400 font-bold">Current price</h4>
                             <h2 className="text-green-400">$30</h2>
                         </div>
-                        <div className="flex bg-gray-300 p-[2rem]">
+                        <div className="flex bg-gray-300 p-[2rem] rounded-b-[10px]">
                             <div className="flex-[0.25]">
-                                <p className="text-dark-gray font-bold">Category - Chemical</p>
-                                <p className="text-dark-gray font-bold">Condition - New</p>
+                                <p className="text-dark-gray font-bold mb-[0.7rem]">Category - Chemical</p>
+                                <p className="text-dark-gray font-bold mb-[0.7rem]">Condition - New</p>
                                 <p className="text-dark-gray font-bold">Quantity - 500ml</p>
                             </div>
                             <div className="flex-[0.75]">
                                 <p className="text-dark-gray">Selling a 500 mL bottle of hydrogen peroxide (H₂O₂) reagent, 5% solution. It’s Sigma-Aldrich brand, still sealed and completely full. I originally bought it for a project but ended up not needing it, so it’s brand new and has been stored properly in a cool, dark space since purchase. Expiry is 2026, so plenty of shelf life left. lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem</p>
-                                Tag1 Tag2 Tag3
+                                <div className="mt-[3rem] flex"><Tag/><Tag/><Tag/></div>
                             </div>
                         </div>
                     </div>
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,7 +3,6 @@ import ReagentView from "./components/composite/reagentview/ReagentView";
 export default function Home() {
   return (
     <div>
-        Homepage
         <ReagentView/>
     </div>
   )
