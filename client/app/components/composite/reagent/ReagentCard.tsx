ab96be9cd69c436a0a06a3de8de110d41cc1d6a4
vi0letchen <violethongjinchen@gmail.com>
2025-09-12 14:31:58 +1200

    feat:reagent card mobile scratch

--- client/app/components/composite/reagent/ReagentCard.tsx
+++ client/app/components/composite/reagent/ReagentCard.tsx
@@ -2,7 +2,7 @@
 import { MapPinIcon } from "@heroicons/react/24/outline"
 import Image from "next/image"
 import Button from "../../generic/button/regular/Button"
-import { ChevronDoubleRightIcon } from "@heroicons/react/24/outline"
+import { ChevronDoubleRightIcon, ClockIcon } from "@heroicons/react/24/outline"
 
 interface ReagentCardProps {
   name: string
@@ -28,55 +28,66 @@ const ReagentCard = ({
   return (
     <div
       className="
-           md:w-[20rem] md:h-[22rem] 
-           w-[15rem] h-[25rem]
+           md:w-[19rem] md:h-[18.5rem] 
+           w-full h-[8rem]
            border-white/30 border-solid border-[1.5px] rounded-xl
            bg-primary/80
        "
     >
-      <div className="m-4 border border-white/30 rounded-lg overflow-hidden relative h-32 drop-shadow-xl">
-        {imageUrl ? (
-          <Image src={imageUrl} fill className="object-cover" alt="" />
-        ) : null}
-        <div className="absolute bottom-0 left-0 right-0 p-2 bg-gradient-to-t from-black/60 to-transparent">
-          <div className="flex flex-wrap gap-1">
-            {tags.map((tag, idx) => (
-              <span
-                key={idx}
-                className="bg-black/30 text-white text-xs px-2 py-1 rounded-lg backdrop-blur-sm"
-              >
-                {tag}
-              </span>
-            ))}
-          </div>
-        </div>
+<div className="flex flex-row md:flex-col m-2 md:m-4 rounded-lg overflow-hidden drop-shadow-xl">
+
+  <div className="relative w-[7rem] h-[6.5rem] md:w-[18rem] md:h-[8rem]">
+    {imageUrl ? (
+      <Image src={imageUrl} fill className="object-cover" alt="" />
+    ) : null}
+    <div className="absolute bottom-0 left-0 right-0 p-2 bg-gradient-to-t from-black/60 to-transparent">
+      <div className="hidden md:block flex flex-wrap gap-1">
+        {tags.map((tag, idx) => (
+          <span
+            key={idx}
+            className="bg-black/30 text-white text-xs px-2 py-1 rounded-lg backdrop-blur-sm"
+          >
+            {tag}
+          </span>
+        ))}
       </div>
-      <span className="flex mx-4 items-center gap-8">
-        {" "}
-        <h3 className="text-white italic">{name}</h3>
-        <p className="text-white/60 italic mt-1">[{formula}]</p>
-      </span>
-      <p className="text-light-gray mx-4">{description}</p>
-      <span className="flex flex-col gap-.5 mx-4 mt-1">
-        <p className="text-gray">Expires</p>
-        <h5 className="text-warning font-bold">{expiryDate}</h5>
-      </span>
-      <div className="bg-light-gray h-[1px] mx-6 my-2"></div>{" "}
-      <div className="items-center flex justify-between md:m-4 m-6">
-        {" "}
-        <p className="underline-offset-2 text-light-gray underline flex text-xs gap-1">
-          <MapPinIcon className="md:w-4 md:h-4 w-3 h-3" />
-          {location}
-        </p>
-        <div className="md:w-[8rem] ml-auto md:translate-x-2">
-          <Button
-            label="Request"
-            size="small"
-            icon={ChevronDoubleRightIcon}
-            iconPosition="right"
-          />
-        </div>
+    </div>
+  </div>
+
+  <div className="flex flex-col justify-between flex-1 px-4 py-2">
+
+    <span className="flex items-center gap-2 md:gap-4">
+      <h3 className="text-white text-xl md:text-2xl italic">{name}</h3>
+      <p className="text-white/60 text-sm md:text-base italic mt-1">[{formula}]</p>
+    </span>
+
+    <span className="flex flex-col mt-1">
+      <p className="flex text-warning text-base md:text-lg">
+      <ClockIcon className="w-5 h-5 mt-0.5 md:mt-1 text-warning" />
+      {expiryDate}      
+      </p>
+
+    </span>
+
+    <div className="hidden md:block bg-light-gray h-[1px] my-2"></div>
+
+    <div className="flex items-center justify-between">
+      <p className="underline-offset-2 text-light-gray underline flex text-xs gap-0.5">
+        <MapPinIcon className="w-4 h-4 mt-0.5" />
+        {location}
+      </p>
+      <div className="w-[8rem] md:translate-x-2">
+        <Button
+          label="Request"
+          size="small"
+          icon={ChevronDoubleRightIcon}
+          iconPosition="right"
+        />
       </div>
+    </div>
+  </div>
+</div>
+
     </div>
   )
 }
