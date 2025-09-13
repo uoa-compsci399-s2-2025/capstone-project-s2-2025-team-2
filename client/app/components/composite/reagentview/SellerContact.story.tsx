3a0f4c00d67b2a4fbe347c6b9451a12361553071
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-09-14 11:53:07 +1200

    feat: create SellerContact story

---
+++ client/app/components/composite/reagentview/SellerContact.story.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/nextjs"
+import SellerContact from "./SellerContact"
+
+const meta = {
+  title: "Example/SellerContact",
+  component: SellerContact,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+  args: {},
+} satisfies Meta<typeof SellerContact>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const DefaultSellerContact: Story = {
+  args: {
+    name: "Violet Chen",
+    location: "University of Auckland",
+    rating: 99
+  },
+}
--- client/app/components/composite/reagentview/SellerContact.tsx
+++ client/app/components/composite/reagentview/SellerContact.tsx
@@ -1,21 +1,29 @@
 import { LuHouse } from "react-icons/lu"
 import { FaRegStar } from "react-icons/fa"
 
-const SellerContact = () => {
+interface SellerContact {
+    name?: string
+    location?: string
+    rating?: number
+}
+
+const SellerContact = ({
+  name, location, rating
+}: SellerContact) => {
   return (
     <div className="flex flex-row items-center">
       <div className="bg-yellow-600 rounded-[50%] flex flex-col justify-center items-center h-[4rem] w-[4rem]">
         PFP
       </div>
       <div className="mx-[1.5rem]">
-        <h4 className="text-midnight text-[1rem] font-bold">Violet Chen</h4>
+        <h4 className="text-midnight text-[1rem] font-bold">{name || "Violet Chen"}</h4>
         <span className="flex items-center text-[0.8rem] text-midnight font-bold">
           <LuHouse className="mr-[0.3rem]" />
-          University of Auckland, City Centre
+          {location || "University of Auckland"}
         </span>
         <span className="text-gold items-center flex text-[0.8rem] font-bold">
           <FaRegStar className="mr-[0.3rem]" />
-          99 (99.9% Positive Feedback)
+          {rating} ({rating}% Positive Feedback)
         </span>
       </div>
       <button className="bg-blue-primary text-white font-sans rounded-[5px]">
