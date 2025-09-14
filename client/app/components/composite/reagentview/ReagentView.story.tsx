60f41e3119a8694a7bce135d2f643e0165508181
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-09-14 13:50:02 +1200

    reagent view story

--- client/app/components/composite/reagentview/ReagentView.story.tsx
+++ client/app/components/composite/reagentview/ReagentView.story.tsx
@@ -16,5 +16,17 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const DefaultReagentView: Story = {
-  args: {},
+  args: {
+    name: "Hydrogen Peroxide 5%",
+    listedDate: "25/09/2025",
+    expiryDate: "25/09/2026",
+    location: "University of Auckland",
+    price: 30,
+    category: "Chemical",
+    condition: "New",
+    quantity: "500ml",
+    description:
+      "Selling a 500 mL bottle of hydrogen peroxide (H₂O₂) reagent, 5% solution. It’s Sigma-Aldrich brand, still sealed and completely full.",
+    tags: ["Tag 1", "Tag 2", "Tag 3"]
+  },
 }
