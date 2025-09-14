0934809bf29e32301785ab7ba3897714de1c67c2
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-09-14 15:36:17 +1200

    commit

--- client/app/components/composite/reagentview/ReagentView.tsx
+++ client/app/components/composite/reagentview/ReagentView.tsx
@@ -83,7 +83,7 @@ const ReagentView = ({
                 <div className="mt-[3rem] flex">
                   {categories?.map((tag) => {
                     return (
-                      <div className="mx-[0.4rem] px-[0.8rem] rounded-[0.7rem] font-family-sans tracking-widest text-white bg-secondary">
+                      <div className="mx-[0.4rem] px-[0.8rem] rounded-[0.7rem] font-family-sans tracking-widest text-white bg-secondary" key={tag}>
                         {tag}
                       </div>
                     )
