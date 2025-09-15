bb990f207213adfaca7a902335f4312421eb14cb
vi0letchen <violethongjinchen@gmail.com>
2025-09-16 01:50:09 +1200

    feat:dark mode for landing page

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import "./globals.css"
+import { Providers } from './providers'
 
 export const metadata: Metadata = {
   title: "Chemical.ly",
@@ -10,14 +11,16 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" >
       <head>
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
-      <body>{children}</body>
+      <body>        <Providers>
+          {children}
+        </Providers></body>
     </html>
   )
 }
