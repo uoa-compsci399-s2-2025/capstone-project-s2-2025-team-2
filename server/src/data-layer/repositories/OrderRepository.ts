79d633974cd7327d257d0199e394b15fd0f4efdf
ZL <ray040406@gmail.com>
2025-09-16 00:42:02 +1200

    feat: CreateOrder service POST function

--- server/src/data-layer/repositories/OrderRepository.ts
+++ server/src/data-layer/repositories/OrderRepository.ts
@@ -11,5 +11,19 @@ export class OrderService {
     const user = await this.userService.getUserById(req.req_id)
     const reagent = await this.reagentService.getReagentById(req.reagent_id)
     if (!user || !reagent) throw new Error("No user or reagent found")
+    const order: Order = {
+      req_id: req.req_id,
+      owner_id: reagent.user_id,
+      reagent_id: req.reagent_id,
+      status: "pending",
+      createdAt: new Date(),
+    }
+    const docRef = await FirestoreCollections.orders.add(order)
+    const createdOrder = {
+      ...order,
+      id: docRef.id,
+    }
+
+    return createdOrder
   }
 }
