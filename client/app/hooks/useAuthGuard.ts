66dfa9e392295ffdbc0f0bc5f4313f3001fed165
ganznz <loganbellinghamlb@gmail.com>
2025-09-18 18:11:27 +1200

    feat: finalise useAuthGuard hook

--- client/app/hooks/useAuthGuard.ts
+++ client/app/hooks/useAuthGuard.ts
@@ -1,89 +1,54 @@
 "use client"
-import { useEffect, useMemo, useState } from "react"
-import { onIdTokenChanged } from "firebase/auth"
+import { useState } from "react"
 import { useRouter } from "next/navigation"
-import { auth } from "@/app/config/firebase"
 import client from "../services/fetch-client"
 
 interface IuseAuthGuard {
   /**
-   * When true, redirects users who aren't logged in to the auth page.
+   * If set to true, users aren't logged in should be redirected to the authentication page.
    * Set to false (default) if the page's content can be viewed by users who aren't logged in.
    */
   redirectToAuth: boolean
 }
 
 // the hook to be exported and consumed, contains functions for usage within components
-const useAuthGuard = (options: IuseAuthGuard = { redirectToAuth: false }) => {
+const useAuthGuard = (options: IuseAuthGuard = { redirectToAuth: true }) => {
   const router = useRouter()
   const authPagePath = "/auth"
 
-  const [authToken, setAuthToken] = useState<string | null>(null)
   const [currentUserUid, setCurrentUserUid] = useState<string | null>(null)
 
-  // onIdTokenChanged functionality
-  useEffect(() => {
-    if (!auth) return
-    const unsubscribe = onIdTokenChanged(auth, async (user) => {
-      // if user exists, refresh token
-      if (user) {
-        const token = await user.getIdToken()
-        localStorage.setItem("authToken", token)
-        setAuthToken(token)
-        setCurrentUserUid(user.uid)
-      } else {
-        localStorage.removeItem("authToken")
-        setAuthToken(null)
-        setCurrentUserUid(null)
-        if (options.redirectToAuth) {
-          router.push(authPagePath)
+  const fetchWithAuth = async <T>(
+    path: string,
+    requestOptions: { protectedEndpoint: boolean } = {
+      protectedEndpoint: true,
+    },
+  ): Promise<{ data: T; error: unknown; response: unknown } | null> => {
+    // Get token directly from localStorage to avoid race conditions
+    const authToken = localStorage.getItem("authToken")
+    const headersObj = requestOptions.protectedEndpoint
+      ? {
+          Authorization: `Bearer ${authToken}`,
         }
-      }
-    })
-    return () => unsubscribe()
-  }, [router])
-
-  // initialise auth token from localstorage if it exists
-  useEffect(() => {
-    const existingAuthToken = localStorage.getItem("authToken")
-    if (existingAuthToken) setAuthToken(existingAuthToken)
-  }, [])
-
-  const fetchWithAuth = async <T>(paths: string): Promise<T | null> => {
-    let res = await client.GET(paths as any, {
-      Authorization: `Bearer ${authToken}`,
+      : {}
+    const { data, error, response } = await client.GET(path as any, {
+      headers: headersObj,
     })
 
-    // if we get 401 error, refresh user auth token and try fetch again
-    if (res?.error?.status === 401) {
-      const user = auth.currentUser
-      if (!user) {
-        if (options.redirectToAuth) {
-          router.push(authPagePath)
-        }
-        return null
-      }
-      const refreshedToken = await user.getIdToken(true)
-      localStorage.setItem("authToken", refreshedToken)
-      setAuthToken(refreshedToken)
-      res = await client.GET(paths as any, {
-        Authorization: `Bearer ${authToken}`,
-      })
-
-      // if we get 401 error again, high chance user isn't logged in -- redirect to auth
-      if (res?.error?.status === 401) {
-        if (options.redirectToAuth) {
-          router.push(authPagePath)
-        }
-        return null
-      }
+    // if we get 401 err (unauthorised) on a protected endpoint, redirect to auth if required
+    if (
+      requestOptions.protectedEndpoint &&
+      response.status === 401 &&
+      options.redirectToAuth
+    ) {
+      router.push(authPagePath)
+      return null
     }
 
-    return res as T
+    return { data, error, response }
   }
 
   return {
-    authToken,
     currentUserUid,
     fetchWithAuth,
   }
