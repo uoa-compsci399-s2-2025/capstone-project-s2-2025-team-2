8e2079772f99821c9724bbb9cac132a0efddc609
Logan <78389560+ganznz@users.noreply.github.com>
2025-09-20 01:12:09 +1200

    Frontend data fetching hook (#76)
    * feat: implement hook for auth & data fetching
    
    * feat: finalise useAuthGuard hook
    
    * feat: hook now properly gets and sets users uid
    
    * nit: separation of concerns - hook no longer associated with logged in users uid
    
    * fix: remove unused import
    
    * fix: ``redirectToAuth`` is now false by default

---
+++ client/app/hooks/useAuthGuard.ts
@@ -0,0 +1,53 @@
+"use client"
+import { useRouter } from "next/navigation"
+import client from "../services/fetch-client"
+
+interface IuseAuthGuard {
+  /**
+   * If set to true, users aren't logged in should be redirected to the authentication page.
+   * Set to false (default) if the page's content can be viewed by users who aren't logged in.
+   */
+  redirectToAuth: boolean
+}
+
+// the hook to be exported and consumed, contains functions for usage within components
+const useAuthGuard = (options: IuseAuthGuard = { redirectToAuth: false }) => {
+  const router = useRouter()
+  const authPagePath = "/auth"
+
+  const fetchWithAuth = async <T>(
+    path: string,
+    requestOptions: { protectedEndpoint: boolean } = {
+      protectedEndpoint: true,
+    },
+  ): Promise<{ data: T; error: unknown; response: unknown } | null> => {
+    // Get token directly from localStorage to avoid race conditions
+    const authToken = localStorage.getItem("authToken")
+    const headersObj = requestOptions.protectedEndpoint
+      ? {
+          Authorization: `Bearer ${authToken}`,
+        }
+      : {}
+    const { data, error, response } = await client.GET(path as any, {
+      headers: headersObj,
+    })
+
+    // if we get 401 err (unauthorised) on a protected endpoint, redirect to auth if required
+    if (
+      requestOptions.protectedEndpoint &&
+      response.status === 401 &&
+      options.redirectToAuth
+    ) {
+      router.push(authPagePath)
+      return null
+    }
+
+    return { data, error, response }
+  }
+
+  return {
+    fetchWithAuth,
+  }
+}
+
+export default useAuthGuard
