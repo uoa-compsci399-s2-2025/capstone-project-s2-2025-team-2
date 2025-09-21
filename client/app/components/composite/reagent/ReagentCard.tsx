74d32aa8d54fa8d12ceab5ca03348c3f639c461f
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-09-21 20:46:13 +1200

    styling:desktop card styling

--- client/app/components/composite/reagent/ReagentCard.tsx
+++ client/app/components/composite/reagent/ReagentCard.tsx
@@ -8,8 +8,8 @@ import {
   CurrencyDollarIcon,
   ArrowsRightLeftIcon,
 } from "@heroicons/react/24/outline"
-import { LuClockAlert } from "react-icons/lu";
-import { IoArrowForward } from "react-icons/io5";
+import { LuClockAlert } from "react-icons/lu"
+import { IoArrowForward } from "react-icons/io5"
 
 interface ReagentCardProps {
   name: string
@@ -47,7 +47,7 @@ const ReagentCard = ({
       className="
             relative
             p-2
-           md:w-[19rem] md:h-[18.5rem] 
+           md:w-[19rem] md:h-full
            w-full
            border-white/30 border-solid border-[1.5px] rounded-xl
        "
@@ -61,12 +61,12 @@ const ReagentCard = ({
       ))}
       <div className="flex flex-row py-auto gap-4 md:gap-0 md:flex-col m-2 md:m-3 rounded-lg overflow-hidden drop-shadow-xl">
         <div className="relative w-[7rem] min-h-[6.5rem] flex items-center justify-center md:w-full md:h-[8rem]">
-            <Image
-                src={imageUrl || "./placeholder.webp"}
-                fill
-                className="object-cover rounded-lg h-full"
-                alt=""
-            />
+          <Image
+            src={imageUrl || "./placeholder.webp"}
+            fill
+            className="object-cover rounded-lg h-full"
+            alt=""
+          />
           <div className="absolute bottom-0 left-0 right-0 p-2 bg-gradient-to-t from-black/60 to-transparent">
             <div className="hidden md:block flex flex-wrap gap-1">
               {tags.map((tag, idx) => (
@@ -82,42 +82,46 @@ const ReagentCard = ({
         </div>
 
         <div className="flex flex-col justify-between flex-1 gap-1 md:py-4">
-          <div className="
+          <div
+            className="
             flex flex-col justify-between
-            ">
-                <div className="
+            "
+          >
+            <div
+              className="
                     md:flex md:gap-1
-                ">
-                    <p className="text-white/60 text-sm md:text-sm italic mt-1 flex gap-1">
-                        {/* Icon */}
-                        {formula === "trade" ? (
-                        <ArrowsRightLeftIcon className="w-5 h-5 text-orange-200" />
-                        ) : formula === "sell" ? (
-                        <CurrencyDollarIcon className="w-5 h-5 text-green-400" />
-                        ) : formula === "giveaway" ? (
-                        <GiftIcon className="w-5 h-5 text-blue-300" />
-                        ) : (
-                        <GiftIcon className="w-5 h-5 text-blue-300" />
-                        )}
-                        {/* Text */}
-                        {formula === "trade" ? (
-                        <span className="text-orange-200 md:hidden">Trade</span>
-                        ) : formula === "sell" ? (
-                        <span className="text-green-400 md:hidden">Sell</span>
-                        ) : formula === "giveaway" ? (
-                        <span className="text-blue-300 md:hidden">Giveaway</span>
-                        ) : (
-                        <span className="text-blue-300 md:hidden">Giveaway</span>
-                        )}
-                    </p>
-                    <span className="flex items-center gap-3 md:gap-4">
-                    <h4 className="text-white text-base md:text-xl">{name}</h4>
-                    </span>
-                </div>
-            
+                "
+            >
+              <p className="text-white/60 text-sm md:text-sm italic mt-1 flex gap-1">
+                {/* Icon */}
+                {formula === "trade" ? (
+                  <ArrowsRightLeftIcon className="w-5 h-5 text-orange-200" />
+                ) : formula === "sell" ? (
+                  <CurrencyDollarIcon className="w-5 h-5 text-green-400" />
+                ) : formula === "giveaway" ? (
+                  <GiftIcon className="w-5 h-5 text-blue-300" />
+                ) : (
+                  <GiftIcon className="w-5 h-5 text-blue-300" />
+                )}
+                {/* Text */}
+                {formula === "trade" ? (
+                  <span className="text-orange-200 md:hidden">Trade</span>
+                ) : formula === "sell" ? (
+                  <span className="text-green-400 md:hidden">Sell</span>
+                ) : formula === "giveaway" ? (
+                  <span className="text-blue-300 md:hidden">Giveaway</span>
+                ) : (
+                  <span className="text-blue-300 md:hidden">Giveaway</span>
+                )}
+              </p>
+              <span className="flex items-center gap-3 md:gap-4">
+                <h4 className="text-white text-base md:text-xl">{name}</h4>
+              </span>
+            </div>
+
             <p className="text-light-gray flex text-xs gap-0.5 mt-[0.1rem] md:hidden">
-                <MapPinIcon className="w-5 h-5" />
-                {location}
+              <MapPinIcon className="w-5 h-5" />
+              {location}
             </p>
             <div className="hidden gap-1">
               {tags.map((tag, idx) => (
@@ -137,37 +141,38 @@ const ReagentCard = ({
           </div>
 
           <div className="hidden md:block bg-light-gray h-[1px] my-2"></div>
-          <p className="text-light-gray text-xs gap-0.5 mt-[0.1rem] hidden md:flex">
-                <MapPinIcon className="w-5 h-5" />
-                {location}
+          <div className="md:flex md:w-full md:justify-between md:items-center">
+            <p className="text-light-gray text-xs gap-0.5 mt-[0.1rem] hidden md:flex">
+              <MapPinIcon className="w-5 h-5" />
+              {location}
             </p>
 
-          <div className="flex items-center justify-between">
-            
-
-            <div className="flex w-full justify-between">
+            <div className="flex items-center justify-between">
+              <div className="flex w-full justify-between items-center">
                 <p className="md:hidden flex text-warning text-sm md:text-base">
-                <LuClockAlert className="w-4.5 h-4.5 mx-[0.1rem] md:mt-0.5 text-warning" />
-                {expiryDate}
-              </p>
-              <div className="md:hidden">
-                <Button
-                  label="View"
-                  textSize="text-xs"
-                  icon={IoArrowForward}
-                  iconPosition="right"
-                  fontWeight="semibold"
-                  className="px-[0.5rem] py-[0.3rem]"
-                />
-              </div>
-              <div className="hidden md:block">
-                <Button
-                  label="View"
-                  textSize="text-sm"
-                  icon={IoArrowForward}
-                  iconPosition="right"
-                  fontWeight="semibold"
-                />
+                  <LuClockAlert className="w-4.5 h-4.5 mx-[0.1rem] md:mt-0.5 text-warning" />
+                  {expiryDate}
+                </p>
+                <div className="md:hidden">
+                  <Button
+                    label="View"
+                    textSize="text-xs"
+                    icon={IoArrowForward}
+                    iconPosition="right"
+                    fontWeight="semibold"
+                    className="px-[0.5rem] py-[0.3rem]"
+                  />
+                </div>
+                <div className="hidden md:block">
+                  <Button
+                    label="View"
+                    textSize="text-sm"
+                    icon={IoArrowForward}
+                    iconPosition="right"
+                    fontWeight="semibold"
+                    className="md:px-[0.4rem] md:py-[0.2rem]"
+                  />
+                </div>
               </div>
             </div>
           </div>
