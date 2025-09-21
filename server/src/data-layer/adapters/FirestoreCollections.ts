f58a374764eae74aac60ae50320e6416b87b7833
Kihyun Kim <166822848+0531Kim@users.noreply.github.com>
2025-09-21 17:31:31 +1200

    Feature inbox (#81)
    * feat: add scrollbar-thin as a global css
    
    * feat: implemented frontend inbox page
    
    * feat: implemented inbox backend features
    
    * feat: implemented inbox chat api
    
    * chore: sorted chat order
    
    * chore: sorted chat order
    
    * chore: checked prettier
    
    * chore: fixed for prettier and lint
    
    * chore: added jwt security to all inbox endpoints

--- server/src/data-layer/adapters/FirestoreCollections.ts
+++ server/src/data-layer/adapters/FirestoreCollections.ts
@@ -1,10 +1,13 @@
 import type { User } from "../../business-layer/models/User"
 import type { Reagent } from "../../business-layer/models/Reagent"
 import { Order } from "../../business-layer/models/Order"
+import { ChatRoom, Message } from "../../business-layer/models/ChatRoom"
 import {
   REAGENTS_COLLECTION,
   USERS_COLLECTION,
   ORDERS_COLLECTION,
+  CHAT_ROOMS_COLLECTION,
+  MESSAGES_COLLECTION,
 } from "./CollectionNames"
 import firestore from "./Firestore"
 
@@ -12,6 +15,8 @@ const FirestoreCollections = {
   users: firestore.collection<User>(USERS_COLLECTION),
   reagents: firestore.collection<Reagent>(REAGENTS_COLLECTION),
   orders: firestore.collection<Order>(ORDERS_COLLECTION),
+  chatRooms: firestore.collection<ChatRoom>(CHAT_ROOMS_COLLECTION),
+  messages: firestore.collection<Message>(MESSAGES_COLLECTION),
 } as const
 
 export default FirestoreCollections
---
+++ server/src/service-layer/dtos/response/ChatRoomResponse.ts
@@ -0,0 +1,15 @@
+import { ChatRoom, Message } from "../../../business-layer/models/ChatRoom"
+
+export interface ChatRoomResponse {
+  chat_room: ChatRoom
+  messages: Message[]
+  other_user: {
+    id: string
+    name: string
+    email: string
+  }
+}
+
+export interface ConversationListResponse {
+  conversations: ChatRoomResponse[]
+}
