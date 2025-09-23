04da02ec38689f948f3870afa429b7e383a8a2b8
ganznz <loganbellinghamlb@gmail.com>
2025-09-23 13:14:36 +1200

    feat: add loading state

--- client/app/profile/[id]/page.tsx
+++ client/app/profile/[id]/page.tsx
@@ -124,6 +124,38 @@ const UserProfile = () => {
     }
   })
 
+  // return loading state if data hasn't finished loading
+  if (!userBeingViewed || reagents.length === 0) {
+    return (
+      <div className="bg-transparent h-[100vh] w-full">
+        <div className="bg-transparent w-full items-center gap-4 text-white flex justify-center mt-[50vh]">
+          <svg
+            fill="white"
+            viewBox="0 0 24 24"
+            width={50}
+            height={50}
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+              opacity=".25"
+            />
+            <path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z">
+              <animateTransform
+                attributeName="transform"
+                type="rotate"
+                dur="0.75s"
+                values="0 12 12;360 12 12"
+                repeatCount="indefinite"
+              />
+            </path>
+          </svg>
+          <h3>Loading...</h3>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <Overlay>
       <div className="px-5">
@@ -175,7 +207,6 @@ const UserProfile = () => {
               <ReagentCard
                 key={uuidv4()}
                 name={reagent.name}
-                description={reagent.description}
                 tags={reagent.categories || []}
                 location={reagent.location || "Unknown"}
                 expiryDate={reagent.expiryDate || "N/A"}
@@ -184,7 +215,7 @@ const UserProfile = () => {
                     ? reagent.images?.[0] || "/placeholder.webp"
                     : "/placeholder.webp"
                 }
-                formula={reagent.tradingType || ""}
+                description={reagent.description || ""}
               />
             ))}
           </div>
