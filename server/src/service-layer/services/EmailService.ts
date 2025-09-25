817d1d880e216754d85b71fb828af8d15b1ed2fb
vi0letchen <violethongjinchen@gmail.com>
2025-09-25 17:50:58 +1200

    feat:email template and sending method

--- server/src/service-layer/services/EmailService.ts
+++ server/src/service-layer/services/EmailService.ts
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer"
 import { EmailTemplates } from "../../utils/EmailTemplates"
+import { Reagent } from "business-layer/models/Reagent"
 
 export default class EmailService {
   private transporter: nodemailer.Transporter
@@ -32,4 +33,31 @@ export default class EmailService {
       throw new Error("Failed to send verification email")
     }
   }
+
+/**   
+ * Sends an email notification to the user about reagents expiring in 30 days.
+   *
+   * @param email - The email address of the user to notify.
+   * @param reagents - An array of reagents that are expiring in 30 days.
+   */
+
+
+  public async sendReagentExpiryNotification(
+    email: string,
+    reagents: Reagent[]): Promise<void> {
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "Reagent Expiry Alert - You have reagents expiring in 30 Days",
+      html: EmailTemplates.getExpiryEmailTemplate(reagents),
+    }
+    try {
+      await this.transporter.sendMail(mailOptions)
+    } catch (error) {
+      console.error("Error sending email:", error)
+      throw new Error("Failed to send reagent expiry notification email")
+    }
+}
+
+
 }
--- server/src/utils/EmailTemplates.ts
+++ server/src/utils/EmailTemplates.ts
@@ -12,4 +12,23 @@ export class EmailTemplates {
       </div>
     `
   }
+  static getExpiryEmailTemplate(reagents: { name: string; expiryDate: string}[]): string {
+    const reagentList = reagents.map(reagent => `
+      <li>
+        <strong>${reagent.name}</strong> - Expiry Date: ${new Date(reagent.expiryDate).toLocaleDateString()}
+      </li>
+    `).join('')
+    
+    return `
+          <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto;">
+        <h2 style="color: #333;">Dear User</h2>
+        <p>You have reagents expiring in 30 days:</p>
+        <div style="background-color: #f4f4f4; padding: 20px; text-align: center; border-radius: 8px; margin: 20px 0;">
+          <h1 style="color: #007bff; font-size: 32px; margin: 0; letter-spacing: 5px;">${reagentList}</h1>
+        </div>
+        <p>Please sell, trade or giveaway those reagents, or use them as soon as possible.</p>
+      </div>
+    `
+}
+
 }
