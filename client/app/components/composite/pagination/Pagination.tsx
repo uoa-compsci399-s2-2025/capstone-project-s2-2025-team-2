3bfcc5f12f30388aaf84bbbe854e4a681e762fbf
vi0letchen <violethongjinchen@gmail.com>
2025-10-01 01:10:58 +1300

    feat:function to get page numbers for styling after

--- client/app/components/composite/pagination/Pagination.tsx
+++ client/app/components/composite/pagination/Pagination.tsx
@@ -9,6 +9,34 @@ export default function Pagination({
   totalPages,
   onPageChange,
 }: PaginationProps) {
+     const getPageNumbers = () => {
+
+     const pages = []
+     const showPaginationStart = currentPage > 2
+     const showPaginationEnd = currentPage < totalPages - 2
+     if (totalPages <=5) {
+          for (let i = 1; i <= totalPages; i++) {
+               pages.push(i)
+          }
+     } else {
+          pages.push(1)
+          if (showPaginationStart) {
+        pages.push('...')
+        const startPage = Math.max(2, currentPage - 1)
+        const endPage = Math.min(totalPages - 1, currentPage + 1)
+
+        for (let i = startPage; i <= endPage; i++) {
+          pages.push(i)
+        }
+        if (showPaginationEnd) {
+          pages.push('...')
+        }
+        pages.push(totalPages)
+      }
+     return pages
+  }
+}
+   const pages = getPageNumbers()         
   return (
     <div>
       <button
