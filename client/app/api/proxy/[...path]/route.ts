3288970c3695f146368d76a229372b256bb911b1
ZL <ray040406@gmail.com>
2025-10-02 00:48:26 +1300

    feat: proxy route

--- client/app/api/proxy/[...path]/route.ts
+++ client/app/api/proxy/[...path]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server"
 
-// const PROXY_BACKEND_URL = "http://54.206.209.62:8000"
-const BACKEND_URL = "http://localhost:8000"
+const BACKEND_URL = "http://54.206.209.62:8000"
+// const BACKEND_URL = "http://localhost:8000"
 
 export async function GET(
   request: NextRequest,
