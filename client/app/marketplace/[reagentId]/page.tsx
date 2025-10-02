4ae3957834bc4b5644c935101614de0ae8cadc56
ganznz <loganbellinghamlb@gmail.com>
2025-10-02 23:54:35 +1300

    feat+fix: reagent view fixes + carousel implementation

--- client/app/marketplace/[reagentId]/page.tsx
+++ client/app/marketplace/[reagentId]/page.tsx
@@ -7,8 +7,9 @@ import { LuClockAlert } from "react-icons/lu"
 import { LuHouse } from "react-icons/lu"
 import Image from "next/image"
 import SellerContact from "@/app/components/composite/reagentview/SellerContact"
-import ReagentBreadcrumb from "@/app/components/composite/reagentview/ReagentBreadcrumb"
 import SellerContactMobile from "@/app/components/composite/reagentview/SellerContactMobile"
+import ImageCarousel from "@/app/components/generic/image_carousel/ImageCarousel"
+import Overlay from "@/app/components/composite/Overlay"
 
 interface ReagentViewProps {
   params: Promise<{ reagentId: string }>
@@ -31,6 +32,7 @@ export default function ReagentView({ params }: ReagentViewProps) {
     fetchReagents()
   }, [])
 
+  // loading state
   if (!reagent)
     return (
       <div className="bg-transparent h-[100vh] w-full">
@@ -60,152 +62,156 @@ export default function ReagentView({ params }: ReagentViewProps) {
         </div>
       </div>
     )
+
+  // content
   return (
-    <div
-      className="
+    <Overlay>
+      <div
+        className="
     font-family-sans h-full w-full min-h-screen md:flex md:items-center md:justify-center
     background-gradient md:p-[2rem] md:-mt-[2rem] mb-[6rem] md:mb-0
     "
-    >
-      <div className="md:max-w-6xl mx-auto w-full">
-        <div className="md:max-w-6xl mx-auto">
-          <div className="md:hidden relative overflow-hidden h-[25rem] md:h-[15rem] md:mb-[1rem]">
-            <Image
-              src="/placeholder.webp"
-              alt="Reagent"
-              fill
-              className="object-cover"
-            />
-          </div>
-          <div className="bg-primary -mt-[1.3rem] mb-[1rem] relative z-10 rounded-t-3xl lg:mt-0 md:mb-0 md:rounded-none md:z-auto md:bg-transparent">
-            <div className="pt-[2rem] lg:pt-0 px-[2rem] md:px-0">
-              <ReagentBreadcrumb reagentName={reagent.name ?? ""} />
+      >
+        <div className="md:max-w-6xl mx-auto w-full">
+          <div className="md:max-w-6xl mx-auto">
+            {/* mobile header img */}
+            <div className="md:hidden relative overflow-hidden h-[25rem]">
+              <Image
+                src={reagent.images?.[0] ?? "/placeholder.webp"}
+                alt="Reagent"
+                fill
+                className="object-cover"
+              />
             </div>
-
-            <div className="flex flex-col items-start">
-              <h1 className="hidden md:block ml-0 mt-[1rem] mb-[1rem] text-white tracking-wider">
-                {reagent.name}
-              </h1>
-              <h2 className="md:hidden ml-[2rem] mr-[2rem] mt-[1.5rem] text-white tracking-wider">
-                <b>{reagent.name}</b>
-              </h2>
-              <div className="md:hidden px-[2rem] mt-[1.5rem]">
-                <h5 className="text-white/80 mb-[0.5rem]">
-                  Quantity: {reagent.quantity}
-                </h5>
-                <p className="text-white mb-[1rem]">
-                  {reagent.description ||
-                    "No description provided for this reagent."}
-                </p>
+            <div className="bg-primary -mt-[1.3rem] relative z-10 rounded-t-3xl lg:mt-0 md:mb-0 md:rounded-none md:z-auto md:bg-transparent">
+              <div className="flex flex-col items-start">
+                {/* reagent name */}
+                <h1 className="w-full text-center text-3xl md:text-4xl ml-0 mt-[2rem] md:mt-0 mb-[1rem] text-white tracking-wider">
+                  {reagent.name}
+                </h1>
+                {/* metadata */}
+                <div className="flex flex-col md:flex-row justify-center items-center w-full gap-3 md:gap-7">
+                  <h5 className="flex text-[#43C05A] dark:text-[#78F58F] items-center text-[0.8rem]">
+                    <FaRegClock className="text-[#43C05A] dark:text-[#78F58F] mr-[0.3rem] w-5 h-5" />{" "}
+                    Listed — {reagent.expiryDate}
+                  </h5>
+                  <h5 className="flex text-[#E5595B] dark:text-[#FF797B] items-center text-[0.8rem]">
+                    <LuClockAlert className="text-[#E5595B] dark:text-[#FF797B] mr-[0.3rem] w-5 h-5" />
+                    Expires — {reagent.expiryDate}
+                  </h5>
+                  <h5 className="flex text-[#58A3E2] dark:text-[#71BEFF] items-center text-[0.8rem]">
+                    <LuHouse className="text-[#58A3E2] dark:text-[#71BEFF] mr-[0.3rem] w-5 h-5" />{" "}
+                    Location — {reagent.location}
+                  </h5>
+                </div>
               </div>
-
-              <div className="px-[2rem] mb-[2rem] md:mb-0 md:px-0 lg:mt-0 gap-[1rem] lg:gap-12 flex flex-col lg:flex-row justify-start md:w-full mt-[1rem]">
-                {" "}
-                <h5 className="flex text-blue-secondary items-center text-[0.8rem]">
-                  <FaRegClock className="text-blue-secondary mr-[0.3rem] w-5 h-5" />{" "}
-                  Listed - {reagent.expiryDate}
-                </h5>
-                <h5 className="flex text-red-500 items-center text-[0.8rem]">
-                  <LuClockAlert className="text-red-500 mr-[0.3rem] w-5 h-5" />
-                  Expires - {reagent.expiryDate}
-                </h5>
-                <h5 className="flex text-blue-secondary items-center text-[0.8rem]">
-                  <LuHouse className="text-blue-secondary mr-[0.3rem] w-5 h-5" />{" "}
-                  Location -{reagent.location}
-                </h5>
-                <div className="md:hidden mt-[1rem] flex">
-                  {reagent.categories?.map((tag) => {
-                    return (
-                      <div
-                        className="px-2 py-1 mr-[1rem] text-xs rounded-lg tracking-widest text-white bg-secondary/30"
-                        key={tag}
-                      >
-                        {tag}
+              <div className="flex md:flex-row flex-col justify-center items-center mt-12 gap-12 md:gap-0">
+                {/* img carousel */}
+                <div className="order-2 md:order-1">
+                  <ImageCarousel
+                    images={
+                      reagent.images?.[0]
+                        ? reagent.images
+                        : [
+                            "/placeholder.webp",
+                            "/placeholder2.webp",
+                            "/placeholder3.webp",
+                            "/placeholder4.webp",
+                          ]
+                    }
+                  />
+                </div>
+                {/* reagent info */}
+                <div className="flex-[1.2] order-1 md:order-2 w-full px-9">
+                  <div className="rounded-[8px] overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.4)]">
+                    <div
+                      className={`text-center py-[1rem] ${
+                        reagent.tradingType === "giveaway"
+                          ? "bg-[#8FCAF4] dark:bg-[#427AA2] dark:text-[#D4EDFF] text-[#327FB5]"
+                          : reagent.tradingType === "sell"
+                            ? "bg-[#9AE39C] dark:bg-[#3F6340] dark:text-[#AFFFB2] text-[#428B44]"
+                            : reagent.tradingType === "trade"
+                              ? "bg-[#FDCC9B] dark:bg-[#92704E] dark:text-[#FFDBB6] text-[#9B7856]"
+                              : "bg-blue-primary/75"
+                      }`}
+                    >
+                      <h4 className="text-lg md:text-xl">Trading Type</h4>
+                      <h2 className="text-2xl md:text-4xl capitalize">
+                        {reagent.tradingType}
+                      </h2>
+                    </div>
+                    <div className="flex flex-col bg-primary p-[2rem] text-center">
+                      <div className="mb-[1rem]">
+                        <h5 className="text-white/80">
+                          Quantity: {reagent.quantity}
+                        </h5>
                       </div>
-                    )
-                  })}
-                </div>
-                <div className="md:hidden mt-[1rem] ">
-                  <div className="w-[60vw] max-w-2xl my-[1rem] border-t border-light-gray" />
-
-                  <h4 className="text-white tracking-wider mb-[0.3rem]">
-                    <b>Seller Details</b>
-                  </h4>
-                  <SellerContactMobile />
+                      <div className="">
+                        <p className="text-white">
+                          {reagent.description ||
+                            "No description provided for this reagent."}
+                        </p>
+                        <div className="mt-[2rem] flex justify-center">
+                          {reagent.categories?.map((tag) => {
+                            return (
+                              <div
+                                className="px-2 py-1 mr-[1rem] text-xs rounded-lg tracking-widest text-white bg-secondary/30"
+                                key={tag}
+                              >
+                                {tag}
+                              </div>
+                            )
+                          })}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
+          </div>
+          {/* spacer div to prevent content from being covered by 'mobile contact seller' section */}
+          <div className="bg-primary md:hidden h-[6rem]" />
 
-            <div className="flex mt-[3rem] justify-center mx-auto">
-              <div className="md:flex-[1.3] relative">
-                <Image
-                  src="/placeholder.webp"
-                  alt="Reagent"
-                  fill
-                  className="object-cover rounded-l-[8px]"
+          {/* mobile contact seller section */}
+          <div className="fixed bottom-0 w-full md:hidden bg-background z-10 rounded-t-3xl">
+            <div
+              className={`flex h-[8rem] ${reagent.tradingType == "sell" ? "justify-between" : "justify-center"}`}
+            >
+              {reagent.tradingType == "sell" && (
+                <span className="flex flex-col justify-center ml-[2rem]">
+                  <p className="text-white/60 text-[1.1rem]">Price</p>
+                  <h2 className="text-green-500">${reagent.price}</h2>
+                </span>
+              )}
+              <div className="flex items-center mr-[2rem]">
+                <SellerContact
+                  reagent={
+                    reagent
+                      ? {
+                          ...reagent,
+                          id: reagentId,
+                          createdAt:
+                            reagent.createdAt instanceof Date
+                              ? reagent.createdAt.toISOString()
+                              : reagent.createdAt,
+                        }
+                      : undefined
+                  }
                 />
               </div>
-              <div className="hidden md:block flex-[1.3] ml-[1rem] ">
-                <div className="rounded-r-[8px] overflow-hidden shadow-[0_4px_6px_rgba(0,0,0,0.4)]">
-                  <div
-                    className={`text-center py-[1rem] ${
-                      reagent.tradingType === "giveaway"
-                        ? "bg-blue-100/75"
-                        : reagent.tradingType === "sell"
-                          ? "bg-green-100/75"
-                          : reagent.tradingType === "trade"
-                            ? "bg-blue-primary/75"
-                            : "bg-blue-primary/75"
-                    }`}
-                  >
-                    <h4 className="text-white">Trading Type</h4>
-                    <h2 className="text-white capitalize">
-                      {reagent.tradingType}
-                    </h2>
-                  </div>
-                  <div className="flex flex-col bg-primary p-[2rem]">
-                    <div className="mb-[1rem]">
-                      <h5 className="text-white/80">
-                        Quantity: {reagent.quantity}
-                      </h5>
-                      {reagent.price && (
-                        <h5 className="text-white/80 mt-2">
-                          Price: ${reagent.price}
-                        </h5>
-                      )}
-                    </div>
-                    <div className="">
-                      <p className="text-white">
-                        {reagent.description ||
-                          "No description provided for this reagent."}
-                      </p>
-                      <div className="mt-[2rem] flex">
-                        {reagent.categories?.map((tag) => {
-                          return (
-                            <div
-                              className="px-2 py-1 mr-[1rem] text-xs rounded-lg tracking-widest text-white bg-secondary/30"
-                              key={tag}
-                            >
-                              {tag}
-                            </div>
-                          )
-                        })}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
-        </div>
 
-        <div className="fixed bottom-0 w-full md:hidden bg-background -mt-[2rem] z-10 rounded-t-3xl">
-          <div className="flex h-[8rem] justify-between">
-            <span className="flex flex-col justify-center ml-[2rem]">
-              <p className="text-white/60 text-[1.1rem]">Price</p>
-              <h2 className="text-green-500">${reagent.price}</h2>
-            </span>
-            <div className="flex items-center mr-[2rem]">
+          {/* widescreen view contact seller section */}
+          <div className="hidden md:block mt-[2rem]">
+            <div className="flex flex-col items-center gap-2">
+              <div className="w-[80vw] max-w-2xl min-w-xl mx-auto my-[1rem] border-t border-light-gray" />
+              <div className="max-w-2xl mx-auto grid grid-cols-4 mb-[0.1rem]">
+                <h4 className="text-left text-white tracking-wider col-span-1">
+                  <b>Seller Details</b>
+                </h4>
+              </div>
               <SellerContact
                 reagent={
                   reagent
@@ -223,32 +229,7 @@ export default function ReagentView({ params }: ReagentViewProps) {
             </div>
           </div>
         </div>
-
-        <div className="hidden md:block mt-[2rem]">
-          <div className="flex flex-col items-center gap-2">
-            <div className="w-[80vw] max-w-2xl min-w-xl mx-auto my-[1rem] border-t border-light-gray" />
-            <div className="max-w-2xl mx-auto grid grid-cols-4 mb-[0.1rem]">
-              <h4 className="text-left text-white tracking-wider col-span-1">
-                <b>Seller Details</b>
-              </h4>
-            </div>
-            <SellerContact
-              reagent={
-                reagent
-                  ? {
-                      ...reagent,
-                      id: reagentId,
-                      createdAt:
-                        reagent.createdAt instanceof Date
-                          ? reagent.createdAt.toISOString()
-                          : reagent.createdAt,
-                    }
-                  : undefined
-              }
-            />
-          </div>
-        </div>
       </div>
-    </div>
+    </Overlay>
   )
 }
