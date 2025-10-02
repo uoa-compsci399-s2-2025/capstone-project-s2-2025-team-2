a4bc86671b8c3349ad3d989d70400c9a4dc66351
del <154120298+del-ereno@users.noreply.github.com>
2025-10-02 21:10:38 +1300

    Mobile marketplace fix (#97)
    * refactor: readded+fixed mobile search bar
    
    * refactor: reusable components for render
    
    * refactor: responsive card + image containers
    
    * refactor: responsive card order/layout
    
    * refactor: desktop footer
    
    * fix: prettier + lint
    
    * fix:card overflow on extreme small screens
    
    * fix:p
    
    * fix:card truncates too early
    
    ---------
    
    Co-authored-by: vi0letchen <violethongjinchen@gmail.com>

--- client/app/components/composite/reagent/ReagentCard.tsx
+++ client/app/components/composite/reagent/ReagentCard.tsx
@@ -19,7 +19,7 @@ const ReagentCard = ({ reagent }: ReagentCardProps) => (
     footerLeft={
       <p className="underline-offset-2 text-white/50 flex text-xs gap-0.5">
         <MapPinIcon className="w-5 h-5" />
-        <span className="truncate max-w-[135px]">
+        <span className="truncate max-w-[10rem]">
           {reagent.location ?? "Unknown"}
         </span>
       </p>
