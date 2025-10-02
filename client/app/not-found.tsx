3f5ae41ae0339bb5095b046fe98db3ae5c4fe2d9
ZL <ray040406@gmail.com>
2025-10-02 13:43:40 +1300

    feat: styling

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -1,9 +1,12 @@
 const NotFound = () => (
-  <div className="w-full h-[100vh] bg-primary flex flex-col justify-center items-center">
+  <div className="w-full h-[100vh] bg-primary flex flex-col justify-center items-center gap-2">
     <h1 className="text-white">404</h1>
-    <h2 className="text-white/50">Page Not Found</h2>
+    <h2 className="text-white/70">Page Not Found</h2>
     <p>Sorry, we couldn't find the page you're looking for.</p>
-    <a href="/" className="text-white underline mt-4">
+    <a
+      href="/marketplace"
+      className="text-white underline mt-4 hover:text-white/80"
+    >
       Go back to marketplace
     </a>
   </div>
