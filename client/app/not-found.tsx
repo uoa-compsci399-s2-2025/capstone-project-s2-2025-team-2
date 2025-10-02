5fe73a4b47a7aa944ef77e915af4c971372a6472
ZL <ray040406@gmail.com>
2025-10-02 13:56:45 +1300

    feat: svg and final styling

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -1,13 +1,19 @@
+import Chemical from "../public/chemical.svg"
+import Button from "./components/generic/button/regular/Button"
+import Image from "next/image"
 const NotFound = () => (
   <div className="w-full h-[100vh] bg-primary flex flex-col justify-center items-center gap-2">
+    <Image src={Chemical} alt="Chemical" className="m-4 w-24 h-24 rotate-180" />
     <h1 className="text-white">404</h1>
     <h2 className="text-white/70">Page Not Found</h2>
     <p>Sorry, we couldn't find the page you're looking for.</p>
-    <a
-      href="/marketplace"
-      className="text-white underline mt-4 hover:text-white/80"
-    >
-      Go back to marketplace
+    <a href="/marketplace" className="mt-4">
+      <Button
+        label="Go back to marketplace"
+        className="hover:bg-blue-primary/75 duration-300 m-2 p-2"
+        textSize="text-sm"
+        fontWeight="semibold"
+      />
     </a>
   </div>
 )
