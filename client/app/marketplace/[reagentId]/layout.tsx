4d831f4501ffc7944746d178aeef2f98f0b58a21
ganznz <loganbellinghamlb@gmail.com>
2025-10-02 23:53:44 +1300

    fix: remove redundant files

--- client/app/marketplace/[reagentId]/layout.tsx
+++
@@ -1,7 +0,0 @@
-import Overlay from "@/app/components/composite/Overlay"
-
-export default function ReagentLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return <Overlay>{children}</Overlay>
-}
