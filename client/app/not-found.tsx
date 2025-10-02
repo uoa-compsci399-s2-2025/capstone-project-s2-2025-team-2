d297eb7c64d7c41c4006daa6e2a40926d34461e4
ZL <ray040406@gmail.com>
2025-10-02 14:09:57 +1300

    feat: responsive mobile text

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -6,7 +6,9 @@ const NotFound = () => (
     <Image src={Chemical} alt="Chemical" className="m-4 w-24 h-24 rotate-180" />
     <h1 className="text-white">404</h1>
     <h2 className="text-white/70">Page Not Found</h2>
-    <p>Sorry, we couldn't find the page you're looking for.</p>
+    <p className="md:w-full w-[300px] text-center">
+      Sorry, we couldn't find the page you're looking for.
+    </p>
     <a href="/marketplace" className="mt-4">
       <Button
         label="Go Back"
