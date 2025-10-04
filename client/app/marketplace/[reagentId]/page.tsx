254488f084f3ad52a0d508fd7e9cfa45cc192041
vi0letchen <violethongjinchen@gmail.com>
2025-10-04 22:52:35 +1300

    feat:createdAt field created

--- client/app/marketplace/[reagentId]/page.tsx
+++ client/app/marketplace/[reagentId]/page.tsx
@@ -18,7 +18,6 @@ export default function ReagentView({ params }: ReagentViewProps) {
   const { reagentId } = use(params)
 
   const [reagent, setReagent] = useState<Reagent | null>(null)
-
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [reagentId])
@@ -92,7 +91,7 @@ export default function ReagentView({ params }: ReagentViewProps) {
                 <div className="flex flex-col md:flex-row justify-center items-center w-full gap-3 md:gap-7">
                   <h5 className="flex text-[#43C05A] dark:text-[#78F58F] items-center text-[0.8rem]">
                     <FaRegClock className="text-[#43C05A] dark:text-[#78F58F] mr-[0.3rem] w-5 h-5" />{" "}
-                    Listed — {reagent.expiryDate}
+                    Listed — {reagent.createdAt ? new Date(reagent.createdAt).toLocaleString() : 'N/A'}
                   </h5>
                   <h5 className="flex text-[#E5595B] dark:text-[#FF797B] items-center text-[0.8rem]">
                     <LuClockAlert className="text-[#E5595B] dark:text-[#FF797B] mr-[0.3rem] w-5 h-5" />
--- server/src/presentation-layer/controllers/UserController.ts
+++ server/src/presentation-layer/controllers/UserController.ts
@@ -187,6 +187,7 @@ export class UserController extends Controller {
     const user_id = request.user.uid
     const data = {
       ...requestObject,
+      createdAt: new Date(),
       user_id,
     }
     const newReagent = await new ReagentService().createReagent(data as Reagent)
