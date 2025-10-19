83cb6ff73fde8fa14d5fad70f3e6b61757a59e04
ZL <ray040406@gmail.com>
2025-10-20 00:50:38 +1300

    feat: fetch orders

--- client/app/history/page.tsx
+++ client/app/history/page.tsx
@@ -1,9 +1,17 @@
 import Overlay from "@/app/components/composite/Overlay"
 import client from "../services/fetch-client"
+import { auth } from "@/app/config/firebase"
+import { components } from "@/models/__generated__/schema"
+
+type Order = components["schemas"]["Order"]
 
 const fetchHistory = async () => {
-  const { data } = await client.GET("/orders")
-  return data
+  const response = await client.GET("/orders", {
+    headers: {
+      Authorization: `Bearer ${await auth.currentUser?.getIdToken()}`,
+    },
+  })
+  return response.data as Order[]
 }
 
 const History = () => {
