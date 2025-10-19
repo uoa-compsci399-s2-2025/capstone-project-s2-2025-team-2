dce4c9430fa6738b3e4104d45cfcb2a9163a723f
ZL <ray040406@gmail.com>
2025-10-20 00:56:43 +1300

    feat: map

--- client/app/history/page.tsx
+++ client/app/history/page.tsx
@@ -1,5 +1,8 @@
+"use client"
 import Overlay from "@/app/components/composite/Overlay"
 import client from "../services/fetch-client"
+import { useState } from "react"
+import { useEffect } from "react"
 import { auth } from "@/app/config/firebase"
 import { components } from "@/models/__generated__/schema"
 
@@ -15,10 +18,59 @@ const fetchHistory = async () => {
 }
 
 const History = () => {
-  const data = fetchHistory()
+  const [orders, setOrders] = useState<Order[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [err, setErr] = useState<string | null>(null)
+
+  useEffect(() => {
+    const loadOrders = async () => {
+      try {
+        setIsLoading(true)
+        const data = await fetchHistory()
+        setOrders(data || [])
+      } catch (err) {
+        setErr("400, failed to fetch orders")
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    loadOrders()
+  }, [])
+  if (isLoading) {
+    return (
+      <Overlay>
+        <div>Loading...</div>
+      </Overlay>
+    )
+  }
+  if (err) {
+    return (
+      <Overlay>
+        <div>{err}</div>
+      </Overlay>
+    )
+  }
   return (
     <Overlay>
-      <div></div>
+      <div>
+        {" "}
+        <div>
+          {orders.length === 0 ? (
+            <p>No orders found</p>
+          ) : (
+            <ul>
+              {orders.map((order, index) => (
+                <li key={index}>
+                  <div>Order: {order.reagent_id}</div>
+                  <div>Initiate Message:{order.message}</div>
+                  <div>Quantity: {order.quantity}</div>
+                  <div>Status: {order.status}</div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
     </Overlay>
   )
 }
