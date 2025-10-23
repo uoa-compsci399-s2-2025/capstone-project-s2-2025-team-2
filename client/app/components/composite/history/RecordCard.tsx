cde6d941e49b901d46f0b14ed24065c81544a573
ZL <ray040406@gmail.com>
2025-10-24 00:27:29 +1300

    feat: storybook component

--- client/app/components/composite/history/RecordCard.tsx
+++ client/app/components/composite/history/RecordCard.tsx
@@ -1,7 +1,17 @@
 "use client"
 
-interface RecordCardProps {}
+interface RecordCardProps {
+  orderId: string
+  status: string
+  createdAt: string
+  offeredReagentId?: string
+  price?: number
+  quantity: number
+  unit: number
+}
 
 const RecordCard = () => {
   return <div></div>
 }
+
+export default RecordCard
