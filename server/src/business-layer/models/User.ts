2f541e251331fa646d587a09304826633484da56
del-ereno <delereno2@gmail.com>
2025-11-06 17:07:17 +1300

    feat: location added to user model

--- server/src/business-layer/models/User.ts
+++ server/src/business-layer/models/User.ts
@@ -3,6 +3,7 @@ export interface User {
   displayName: string
   lastName?: string
   university: string
+  location?: string
   about?: string
   role: "user" | "lab_manager" | "admin"
   image?: string
